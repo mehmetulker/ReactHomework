b289a98936e7f646e2a5bc69efa9cddc2f0c66f7
mehmetulker <mehmet-ulker@hotmail.com>
2023-11-14 13:47:13 +0300

    React app

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-
+// var slugify = require('slugify')
+// import slugify from "slugify";
+// Default import varsa
+// import topla,{hello,cıkar} from "./my-module.js";
+import {topla,hello,cıkar,text,user,users} from "./my-module.js";
 import  getData  from "./app.js";
+// const title=slugify("Mehmet ülker bu bağımllıklarım Test ekleme ")
+// console.log(topla(55,47));
+// console.log(cıkar(55,47));
+// hello("Mehmet");
+// console.log(text);
+// console.log(user);
+// console.log(users);
+
 getData(1);
